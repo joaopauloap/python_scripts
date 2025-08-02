2faab408c41faa13c57d68262ce6a456a875b55c
João Paulo de Araújo Porto <36867356+joaopauloap@users.noreply.github.com>
2025-08-02 14:57:38 -0300

    Update selenium-download-fs-images.py

--- selenium-download-fs-images.py
+++ selenium-download-fs-images.py
@@ -1,31 +1,18 @@
 import undetected_chromedriver as uc
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 import time
-import os
 
 # Parametros
 USERNAME = ""
 PASSWORD = ""
 URL = "" 
-COUNT = 100
-EXTENSION_FOLDER_NAME = "fs"
+COUNT = 10
 
-# Diretório atual onde está o script
-dir_atual = os.path.dirname(os.path.abspath(__file__))
-
-# Caminho completo da pasta da extensão 
-path_extension = os.path.join(dir_atual, EXTENSION_FOLDER_NAME)
-
-# Criando opções do Chrome
-options = uc.ChromeOptions()
-options.add_argument(f"--load-extension={path_extension}")
-
-# Criando o driver com a extensão
-driver = uc.Chrome(options=options)
+# Inicia navegador com proteção contra bloqueio
+driver = uc.Chrome()
+driver = uc.Chrome()
 wait = WebDriverWait(driver, 15)
 short_wait = WebDriverWait(driver, 3)
 
@@ -43,14 +30,7 @@ field_password.send_keys(PASSWORD)
 button_login.click()
 
 # Aguarda para redirecionamento
-time.sleep(10)
-
-try:
-	button_ok = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Muito bem']")))
-	button_ok.click()
-	time.sleep(1)
-except Exception:
-        pass
+time.sleep(5)
 
 # ===== LOOP DE DOWNLOAD =====
 for i in range(COUNT):
@@ -66,18 +46,19 @@ for i in range(COUNT):
 
     # Clica no botão de confirmação no modal
     try:
-        radio = short_wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='radio'][value='JPG Only']")))
-        radio.click()
-        time.sleep(1)
         confirm_button = short_wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="full-text-confirm-download"]')))
         confirm_button.click()
     except Exception:
         pass
     
-    # Próxima imagem
-    time.sleep(10)
-    body = driver.find_element(By.TAG_NAME, "body")
-    body.send_keys(Keys.ARROW_RIGHT)
+    # Clica no botão Próxima imagem
+    try:
+        time.sleep(5)
+        next_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label="Próxima Imagem"]')))
+        next_button.click()
+    except Exception:
+        print("Error: Next button not found!")
+        break
 
 
 driver.quit()
