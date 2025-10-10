245a5f742f920f86607db89a59b94289df82f0a6
João Paulo de Araújo Porto <36867356+joaopauloap@users.noreply.github.com>
2025-10-10 14:50:50 -0300

    Update selenium-download-fs-images.py

--- selenium-download-fs-images.py
+++ selenium-download-fs-images.py
@@ -1,18 +1,31 @@
 import undetected_chromedriver as uc
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import time
+import os
 
 # Parametros
 USERNAME = ""
 PASSWORD = ""
 URL = "" 
-COUNT = 10
+COUNT = 100
+EXTENSION_FOLDER_NAME = "fs"
 
-# Inicia navegador com proteção contra bloqueio
-driver = uc.Chrome()
-driver = uc.Chrome()
+# Diretório atual onde está o script
+dir_atual = os.path.dirname(os.path.abspath(__file__))
+
+# Caminho completo da pasta da extensão 
+path_extension = os.path.join(dir_atual, EXTENSION_FOLDER_NAME)
+
+# Criando opções do Chrome
+options = uc.ChromeOptions()
+options.add_argument(f"--load-extension={path_extension}")
+
+# Criando o driver com a extensão
+driver = uc.Chrome(options=options)
 wait = WebDriverWait(driver, 15)
 short_wait = WebDriverWait(driver, 3)
 
@@ -35,9 +48,16 @@ try:
     button_cookies.click()
 except Exception as e:
     print(f"Error: accept cookies button not found!")
-
+    
 # Aguarda para redirecionamento
 time.sleep(5)
+
+try:
+	button_ok = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Muito bem']")))
+	button_ok.click()
+	time.sleep(1)
+except Exception:
+        pass
 
 # ===== LOOP DE DOWNLOAD =====
 for i in range(COUNT):
@@ -53,12 +73,15 @@ for i in range(COUNT):
 
     # Clica no botão de confirmação no modal
     try:
+        radio = short_wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='radio'][value='JPG Only']")))
+        radio.click()
+        time.sleep(1)
         confirm_button = short_wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="full-text-confirm-download"]')))
         confirm_button.click()
     except Exception:
         pass
     
-    # Clica no botão Próxima imagem
+# Clica no botão Próxima imagem
     try:
         time.sleep(5)
         next_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[aria-label="Próxima Imagem"]')))
